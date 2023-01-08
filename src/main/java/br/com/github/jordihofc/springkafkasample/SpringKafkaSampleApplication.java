fd8ca59a8e05d190be4b6b5af6ab0e1faad112ec
JordiHOFC <jordyhenrique7@gmail.com>
2023-01-08 10:54:39 -0300

    initial commit

---
+++ src/main/java/br/com/github/jordihofc/springkafkasample/SpringKafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.jordihofc.springkafkasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/github/jordihofc/springkafkasample/SpringKafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.jordihofc.springkafkasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
